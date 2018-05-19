a00cbb71018e973828654c2ffb48ca58e5dea584
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-18 22:16:32 -0400

    iss icon on map

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,44 @@ router.get('/', function(req, res, next) {
         lat: json.iss_position.latitude,
         lng: json.iss_position.longitude
       } 
-      
       console.log('iss: ', loc);
-      res.render('index', { title: 'Express', googleMapsKey: config.googleMapsKey, lat: loc.lat, lng: loc.lng });
-    });
-}); // end router get
+      return loc;
+    })
+    .then(loc => {
+      T.get('search/tweets', { q: `geocode:"${loc.lat} ${loc.lng} 200mi"`, count: 100 }, function(err, data, response) {
+        // note: the coordinates returned by twitter is in the order lng, lat (even though the search
+        // is for lat, lng)
+        let tweets = [];
+        let tweetsWithoutCoordinates = [];
 
-module.exports = router;
+        data.statuses.forEach(tweet => {
+          console.log('tweet.coordinates: ', tweet.coordinates);
+          if (tweet.coordinates) { // check to see if there are coordinates 
+            tweets.push({
+              text: tweet.text, 
+              user: tweet.user.screen_name, 
+              lat: tweet.coordinates.coordinates[1],
+              lng: tweet.coordinates.coordinates[0]
+            });
+          } else {
+            tweetsWithoutCoordinates.push({
+              text: tweet.text, 
+              user: tweet.user.screen_name
+            });
+          } // end if tweets.coordinates
+        });
+
+        console.log(tweets);
+        
+        res.render('index', { title: 'Express', googleMapsKey: config.googleMapsKey, lat: loc.lat, lng: loc.lng });
+      });
+    });
+  
+  
+  
+  
+  
+  
+  }); // end router get
+  
+  module.exports = router;
