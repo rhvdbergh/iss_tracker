f6cfa288ab9fa9cf9595b74b5aedce4eda443d71
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-18 20:16:19 -0400

    tracks iss, displays map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,6 @@
 var map;
 
+// this script initializes the Google Map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const config = require('../bin/config.js');
+const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', googleMapsKey: config.googleMapsKey });
-});
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then(res => res.json())
+    .then(json => {
+      const loc = {
+        lat: json.iss_position.latitude,
+        lng: json.iss_position.longitude
+      } 
+      
+      console.log('iss: ', loc);
+      res.render('index', { title: 'Express', googleMapsKey: config.googleMapsKey, lat: loc.lat, lng: loc.lng });
+    });
+}); // end router get
 
 module.exports = router;
